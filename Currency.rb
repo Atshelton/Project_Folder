cb24402acf0445919e45b15a04952e0bbfa25c77
Alex Shelton <tbfunkymonkey@gmail.com>
2016-06-24 13:59:28 -0400

    Figured out what was not needed and removed it starting on the adding and subtracting metods

--- Currency.rb
+++ Currency.rb
@@ -1,24 +1,34 @@
 class Currency
 
+
   def initialize(total_value, currency_type)
     @total = total_value                      # => 50, 50, 50
     @type = currency_type                     # => "dollars", "yen", "euro"
   end                                         # => :initialize
 
-def total
-  return @total
-end              # => :total
+  def total
+    return @total
+  end              # => :total
+  
+  def type
+    return @type
+  end             # => :type
 
-def equal
-  return @currency_equality  # => nil
-end                          # => :equal
+  def == (other_type)
+    @total == other_type.total_value && @type == other_type.currency_type  # ~> NoMethodError: undefined method `total_value' for #<Currency:0x007ffdc3825be0 @total=50, @type="yen">
+  end                                                                      # => :==
 
-def type
-  return @type
-end             # => :type
 
-end  # => :type
+end  # => :==
 
-american_money = Currency.new(50,"dollars")  # => #<Currency:0x007f811b023b88 @total=50, @type="dollars">
-japanese_money = Currency.new(50,"yen")      # => #<Currency:0x007f811b023200 @total=50, @type="yen">
-european_money = Currency.new(50,"euro")     # => #<Currency:0x007f811b022a08 @total=50, @type="euro">       # => false
+american_money = Currency.new(50,"dollars")  # => #<Currency:0x007ffdc3826e78 @total=50, @type="dollars">
+japanese_money = Currency.new(50,"yen")      # => #<Currency:0x007ffdc3825be0 @total=50, @type="yen">
+european_money = Currency.new(50,"euro")     # => #<Currency:0x007ffdc38248a8 @total=50, @type="euro">
+
+american_money. == (japanese_money)
+
+# ~> NoMethodError
+# ~> undefined method `total_value' for #<Currency:0x007ffdc3825be0 @total=50, @type="yen">
+# ~>
+# ~> /Users/alexshelton/Desktop/Projects_Folder/Currency.rb:22:in `=='
+# ~> /Users/alexshelton/Desktop/Projects_Folder/Currency.rb:32:in `<main>'
